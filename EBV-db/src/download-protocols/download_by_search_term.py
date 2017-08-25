7dd7c8d25fa569a6ac43d9528ff4de6078328c49
ariel <arieljberenstein@gmail.com>
2017-08-25 17:44:54 -0300

    add search term - ids mapp

--- EBV-db/src/download-protocols/download_by_search_term.py
+++ EBV-db/src/download-protocols/download_by_search_term.py
@@ -20,15 +20,23 @@ search_term = args.search_term
 outputpath = args.outputpath
 RetMax = args.retmax
 
-handle = Entrez.esearch(db='nucleotide', term=search_term,RetMax = RetMax)
-genome_ids = Entrez.read(handle)['IdList']
+def main():
+    handle = Entrez.esearch(db='nucleotide', term=search_term,RetMax = RetMax)
+    genome_ids = Entrez.read(handle)['IdList']
+    file_search_mapp =  outputpath + 'idList'+  search_term + '.txt'
 
+    with open(file_search_mapp, 'w') as g:
+        g.write('search_term : {} \n'.format(search_term))
+        g.write("\n".join(genome_ids))
+        g.write("\n")
 
-for genome_id in genome_ids:
-    record = Entrez.efetch(db="nucleotide", id=genome_id, rettype="gb", retmode="text")
+    for genome_id in genome_ids:
+        record = Entrez.efetch(db="nucleotide", id=genome_id, rettype="gb", retmode="text")
 
-    filename = '{}genBankRecord_{}.gb'.format(outputpath,genome_id)
-    print('Writing:{}'.format(filename))
-    with open(filename, 'w') as f:
-        f.write(record.read())
+        filename = '{}genBankRecord_{}.gb'.format(outputpath,genome_id)
+        print('Writing:{}'.format(filename))
+        with open(filename, 'w') as f:
+            f.write(record.read())
         
+if __name__ == "__main__":
+    main()
