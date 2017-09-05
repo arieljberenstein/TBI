46e38bafc50c27865b3606613663be4afc14b6b6
arieljberenstein <arieljberenstein@gmail.com>
2017-09-05 09:34:07 -0300

    Update makeMSA.sh

--- EBV-db/src/makeMSA.sh
+++ EBV-db/src/makeMSA.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-
+seqpath = '/data/EBV/byACCIDs/'
 for i in {'AllseqsUnderto150k_withRef','test_secs'};
 do
+# create output folder 
+mkdir /data/EBV/msas/$i/
+outpath = /data/EBV/msas/$i/
 ## harcoded paths to outputfiles
 echo merging fasta files for group $i;
 input=/data/EBV/seqGroups/$i.txt
-outfa=/data/EBV/multiple_fastas/$i.fa;
-outclust=/data/EBV/msas/$i.clustal;
+outfa=$outpath$i.fa;
+outclust=$outpath$i.clustal;
 
-sudo python /home/ariel/repo/TBI/EBV-db/src/cat-fasta-files.py --idfile=$input --path='/data/EBV/byACCIDs/' --outputfile=$outfa;
+sudo python /home/ariel/repo/TBI/EBV-db/src/cat-fasta-files.py --idfile=$input --path=$seqpath --outputfile=$outfa;
 echo runing msa-mafftfor group $i;
 sudo mafft --clustalout --thread 4 $outfa > $outclust;
+
+echo runing rate4site
+sudo rate4site -s $outclust -t $outclust -o $outpath$i_rate4site.res
+
 done
 
 
