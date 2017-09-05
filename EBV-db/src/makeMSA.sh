4a5639206241aad478c7f6c78fcda15315d42f24
arieljberenstein <arieljberenstein@gmail.com>
2017-09-05 10:22:17 -0300

    Update makeMSA.sh

--- EBV-db/src/makeMSA.sh
+++ EBV-db/src/makeMSA.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 seqpath = '/data/EBV/byACCIDs/'
-for i in {'AllseqsUnderto150k_withRef','test_secs'};
+for i in "$@";
 do
+fname=$(basename $i) # remove path
+fbname=${fname%.*}   # remove extension
+
 # create output folder 
-mkdir /data/EBV/msas/$i/
-outpath = /data/EBV/msas/$i/
+mkdir /data/EBV/msas/$fbname/
+outpath = /data/EBV/msas/$fbname/
 ## harcoded paths to outputfiles
-echo merging fasta files for group $i;
-input=/data/EBV/seqGroups/$i.txt
-outfa=$outpath$i.fa;
-outclust=$outpath$i.clustal;
+echo merging fasta files for group $fbname;
+input=/data/EBV/seqGroups/$fbname.txt
+outfa=$outpath$fbname.fa;
+outclust=$outpath$fbname.clustal;
 
 sudo python /home/ariel/repo/TBI/EBV-db/src/cat-fasta-files.py --idfile=$input --path=$seqpath --outputfile=$outfa;
-echo runing msa-mafftfor group $i;
+echo runing msa-mafftfor group $fbname;
 sudo mafft --clustalout --thread 4 $outfa > $outclust;
 
 echo runing rate4site
-sudo rate4site -s $outclust -t $outclust -o $outpath$i_rate4site.res
+sudo rate4site -s $outclust -t $outclust -o $outpath$fbname_rate4site.res
 
 done
 
