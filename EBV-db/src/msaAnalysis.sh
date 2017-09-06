1bfb6a6a34055e3d9f75d4c4ddc08514ca5db27d
arieljberenstein <arieljberenstein@gmail.com>
2017-09-06 15:31:42 -0300

    pipeline

--- EBV-db/src/msaAnalysis.sh
+++ EBV-db/src/msaAnalysis.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+trim=0.25
 for f in "$@";
     do
     fname=$(basename $f); # remove path
@@ -5,6 +8,12 @@ for f in "$@";
     
     inputpath=/data/EBV/msas/$fbname/;
     msafile=$inputpath$fbname.clustal; 
-    print $msafile;
+    trimmedout=$inputpath$fbname'_trimmed'$trim'.clustal'
+    mapptrim=$inputpath'colnumb.txt'
+
+    echo $trimmedout 
+    #echo $msafile;
+
+    trimal -in $msafile -out $trimmedout -gt $trim -colnumbering > $mapptrim
     done
 #trimal -in ./fullrandseqs_test/fullrandseqs_test.clustal -out ./fullrandseqs_test/trimed.clustal -gt 0.25 -colnumbering > ./fullrandseqs_test/colnumb.txt
