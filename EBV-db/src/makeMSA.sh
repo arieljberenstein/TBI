7bf114791d0ff54d855e1a64af90c057e6f663a5
arieljberenstein <arieljberenstein@gmail.com>
2017-09-11 12:19:39 -0300

    make msa with gap penalization

--- EBV-db/src/makeMSA.sh
+++ EBV-db/src/makeMSA.sh
@@ -1,27 +1,57 @@
 #!/bin/bash
 
-seqpath='/data/EBV/byACCIDs/'
-for i in "$@";
+
+for i in "$@"
 do
-fname=$(basename $i) # remove path
+case $i in
+    -f=*|--file=*)
+    file="${i#*=}"
+
+    ;;
+    -g=*|--gap=*)
+    gap="${i#*=}"
+    
+    ;;
+    
+    *)
+            # unknown option
+    ;;
+esac
+    
+done
+
+echo $gap
+echo $file
+
+
+seqpath='/data/EBV/byACCIDs/'
+fname=$(basename $file) # remove path
 fbname=${fname%.*}   # remove extension
+
 
 # create output folder 
 mkdir /data/EBV/msas/$fbname/
 outpath=/data/EBV/msas/$fbname/
+
 ## harcoded paths to outputfiles
 echo merging fasta files for group $fbname;
 input=/data/EBV/seqGroups/$fbname.txt
 outfa=$outpath$fbname.fa;
-outclust=$outpath$fbname.clustal;
+outmsa=$outpath$fbname'_msa_gap'$gap.fa;
+#echo $outclust
 
 sudo python /home/ariel/repo/TBI/EBV-db/src/cat-fasta-files.py --idfile=$input --path=$seqpath --outputfile=$outfa;
 echo runing msa-mafftfor group $fbname;
-sudo mafft --clustalout --thread 4 $outfa > $outclust;
+
+#sudo mafft --clustalout --thread 4 $outfa > $outclust;
+
+# this option is accurate but its take a lot of memmory and time. For only 6 full EBV sequences it take 30Gb of memory at the very begining! 
+#sudo mafft --localpair --lop -10 --lexp -0.1  $outfa > $outmsa
+
+## here we are using the default seting --6merpair, where distance is computed based on 6mers. Nevertheless we are changing the open penalty score
+sudo mafft --op $gap  $outfa > $outmsa
 
 #echo runing rate4site
 #sudo rate4site -s $outclust -o $outpath$fbname_rate4site.res
-
-done
 
 
