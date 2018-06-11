e19bd27f1c294fdba14af15c4ea0bd609cf4c528
ariel <arieljberenstein@gmail.com>
2018-06-11 11:54:36 -0300

    get saplepath for lane files

--- NGS/pipeline/lanes/lanes.py
+++ NGS/pipeline/lanes/lanes.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import argparser 
+import fnmatch
+
 
 def parseargs():
     parser = argparse.ArgumentParser(description='generate lane file paths by sample')
@@ -9,10 +11,39 @@ def parseargs():
     args = parser.parse_args()
     return args
 
+def get_samplenames():
+    samplenames = [os.path.basename(f).split(sep)[0] for f in files]
+    samplenames = list(set(samplenames)) # drop duplicates
+    return samplenames
     
-if __name__ = '__main__':
+
+
+def recursive_glob(treeroot, pattern):
+    results = []
+    for base, dirs, files in os.walk(treeroot):
+        goodfiles = fnmatch.filter(files, pattern)
+        results.extend(os.path.join(base, f) for f in goodfiles)
+    return results
+
+if __name__ == '__main__':
     args = parseargs()
     fastq_folder, sep = args.fastq_folder,args.sep
     files = glob.glob(fastq_folder+'*')
-    samplenames = [f.split(sep)[0] for f in files]
-    print samplenames
+    samplenames = get_samplenames()
+    outputfolder = '%slanes_samplepath/'%fastq_folder
+    if not os.path.exists(outputfolder):
+        os.mkdir(outputfolder)        
+
+    for sample in samplenames:
+        r1 = recursive_glob(treeroot= fastq_folder, pattern='%s*R1*gz'%sample)
+        r2 = recursive_glob(treeroot= fastq_folder, pattern='%s*R2*gz'%sample)
+        with open(outputfolder+sample+'_R1.txt','wb') as fr1:
+            for line in r1:
+                fr1.write(line+'\n')
+        fr1.close()        
+        with open(outputfolder+sample+'_R2.txt','wb') as fr2:
+            for line in r2:
+                fr2.write(line+'\n')
+        fr2.close()
+            
+        
