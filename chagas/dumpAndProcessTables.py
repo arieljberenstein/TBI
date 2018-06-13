b01baf60eb8c1977de1fc0d75d8e6a6d85b50c95
ariel <arieljberenstein@gmail.com>
2018-06-13 16:33:38 -0300

    process tables on chagas db

--- chagas/dumpAndProcessTables.py
+++ chagas/dumpAndProcessTables.py
@@ -2,6 +2,7 @@
 import argparse
 import sys, subprocess, os
 import pandas as pd
+import glob
 
 
 
@@ -14,15 +15,12 @@ def get_args():
     return args
 
 
+# code addapted from AccessDump.py file
+# A simple script to dump the contents of a Microsoft Access Database.
+# It depends upon the mdbtools suite:
+#   http://sourceforge.net/projects/mdbtools/
+# Dump the schema for the DB
 def DumpDb(imput_db,dump_path):
-#    ..... aca hay que bajar este codigo ---> #python ./AccessDump.py $1 | sqlite3 $1.sqlite
-
-    # AccessDump.py
-    # A simple script to dump the contents of a Microsoft Access Database.
-    # It depends upon the mdbtools suite:
-    #   http://sourceforge.net/projects/mdbtools/
-
-    # Dump the schema for the DB
     subprocess.call(["mdb-schema", imput_db, "mysql"])
 
     # Get the list of table names with "mdb-tables"
@@ -51,14 +49,14 @@ def DumpDb(imput_db,dump_path):
             f.write(tc)
         f.close()
     
-
-            
     print "COMMIT;" # end the transaction
     sys.stdout.flush()
 
 
     #os.system('for x in `mdb-tables -1 %s`; do mdb-export %s $x >> %s.$x.csv ; done'%(input_db,input_db,dump_path))
     #os.system('mv %s.sqlite %s'%(input_db,dump_path))
+
+
 
 # create output directories    
 def create_output_dirs(opath):
@@ -88,7 +86,136 @@ def JoinSerotipeTables(dump_path):
     serototal.to_csv(dump_path+'Serologias.csv')
     subprocess.call(['rm',sero1])
     subprocess.call(['rm',sero2])
+
+def get_codefiles(dump_path):
+    codefiles = glob(dump_path+'*[{c,C}]ode*csv')
+    codepath = dump_path+'codefiles/'
+    if not os.path.exists(codepath):
+        os.mkdir(codepath)
+    for f in codefiles:
+        subprocess.call(['cp',f,codepath])
+    return codefiles
+
+
+##########################################
+#### internal functions for parse_raw_data
+##########################################
+def remove_numbers(s):
+    return(''.join([i for i in s if not i.isdigit()]))
+
+def remove_last_two_characters(s):
+    return s[:-2]
+
+def get_last_two_characters(s):
+    return s[-2:]
+
+
+def load_data(inputfile):
+    data = pd.read_csv(inputfile)
+    primary = data.columns[0] 
+    data.set_index(primary,inplace = True)
+    return(data)
     
+
+## detecto numero de visitas de la tabla    
+def get_visitas(data,tabla,column_clases):
+    if(tabla == 'EvAdversos'):
+        nmax =  int(data.shape[1])/int(len(column_clases)-1)
+        n_visita = [str(i+1).zfill(1) for i in range(nmax)]
+    else:
+        nmax =  int(data.shape[1])/int(len(column_clases)-1)
+        n_visita = [str(i).zfill(1) for i in range(nmax-1)]
+    return(n_visita)
+
+
+def broad_to_long(table,column_clases,n_visita,dropna = True):
+    long_format = []
+    cols = pd.Series(table.columns)
+
+    for n in n_visita:
+        visita_ith = [c+n for c in column_clases]
+        index = range(len(visita_ith))
+        
+        #indice de columnas que sobreviven
+        ii = [i for i in index if visita_ith[i] in cols.values]
+        visit_i = pd.Series(visita_ith)[ii]
+        newc = pd.Series(column_clases)[ii]
+        
+        daux =  table[visit_i]
+        daux.columns = newc
+        daux['numero_visita'] = n
+        long_format.append(daux)         
+    long_format = pd.concat(long_format)
+    
+    if dropna:
+        long_format.dropna(subset=column_clases,inplace = True,how='all')
+    
+    return(long_format)
+
+
+##########################################
+##########################################
+
+
+
+def parse_raw_data(tablename, tablefile,proc_path):
+    col_dict = {  # this dictionary define columns to be parsed in each table. 
+    'Cardio': ['Ecg', 'FechaCardio', 'ECGDescrip', 'Eco', 'FechaEco',
+                'Ecodescrip'],
+
+    'Clinica': ['ExClinico', 'Piel', 'Ojo', 'ORL', 'Muculo', 'Cardio', 'Resp',
+                'Gastro', 'Higado', 'SNC', 'Metabol', 'Endoc', 'Uro', 'Hemato',
+                'Otros'],
+
+    'EvAdversos': ['Ev', 'NumVisitaEV', 'CodigoEA', 'quetto', 'DescripEA', 'InicioEA',
+                'FinalEA', 'SeveridadEA', 'RelacionEA', 'TratEA', 'EvolEA'],
+
+    'Laboratorio': ['Lab', 'LabGral', 'Hb', 'Hto', 'Blancos', 'Cayados', 'Neutrofilos',
+                'Eos', 'Bas', 'Linfo', 'Mono', 'Plaquetas', 'Bitotal', 'Bidirecta',
+                'TGO', 'TGP', 'Proteinas', 'Albumina', 'Colesterol', 'Falcalina',
+                'Creatinina'],
+                
+    'Parasitemia': ['FechaPar', 'Mh', 'Pcr', 'Xeno', 'IRL', 'Hemoc','Parasit','Tecnica'], 
+
+    'Serologia': ['Serologia', 'Elisa', 'TitElisa', 'Hai', 'TituloHai', 'AD',
+                'TitAD', 'AP', 'TitAP', 'F23', 'TitF23', 'Antiidiotipo'],
+            
+    'Serologia_joined': ['Serologia', 'Elisa', 'TitElisa', 'Hai', 'TituloHai', 'AD',
+                        'TitAD', 'AP', 'TitAP', 'F23', 'TitF23', 'Antiidiotipo'],
+    }
+    data = load_data(tablefile)    
+    column_clases = col_dict[tablename]
+    n_visita = get_visitas(data,tablename,column_clases)
+    longtable = broad_to_long(data,column_clases,n_visita,dropna = True)
+    return longtable 
+
+
+
+
+#this function requiere parse_raw_data function
+def process_tables(dump_path,proc_path):
+    tables = ['Serologia_joined','Laboratorio','Clinica','EvAdversos','Parasitemia','Cardio','Demografico','Pacientes','Tratamiento']
+    tables_pass_true = ['Demografico','Tratamiento','Pacientes']
+
+    for tablename in tables:
+        tablefile = dump_path + tablename + '.csv'
+        output_table_file = proc_path + tablename + '.csv'
+        
+        if tablename not in tables_pass_true:
+            #os.system('python /home/ariel/Projects/Gutierrez/chagas/dropbox_folder/src/parse_raw_data.py -i %s -o %s -t %s'%(inputf,opath,t))
+            processedtable = parse_raw_data(tablename = tablename,tablefile = tablefile)
+            processedtable.to_csv(output_table_file,sep = '|',encoding='utf-8')
+        else:
+            df = pd.read_csv(tablefile)
+            df.to_csv(output_table_file,sep="|",index = False)
+    
+#def get_tablefiles(dump_path):
+#    codefiles = glob(dump_path+'*[{c,C}]ode*csv')
+#    tf = glob(dump_path+'*csv')
+#    tablefiles = [t for t in tf if tf not in codefiles]    
+#    return tablefiles
+
+
 
 if __name__ == '__main__' : 
     args = get_args()
@@ -104,5 +231,13 @@ if __name__ == '__main__' :
     #esto reemplaza el scrppython join_sertotipe_tables.py 
     JoinSerotipeTables(dump_path)
 
+    #code_files: detect and save:
+    codefiles = get_codefiles(dump_path)
+    
+    process_tables(tables,tables_pass_true,proc_path)
+    
 
     #python transformDB.py # este file llama a parse_raw_data.py que es el que tiene la papota, cualquier ascepcion de procesamiento debe modificarse en 'parse_raw_data.py'
+
+
+
